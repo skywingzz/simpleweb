08e7bd3944038174c215f1e8bde13880ea0c524b
배성렬/Front개발팀/SKP <skywing@sk.com>
2018-01-08 16:26:38 +0900

    first commmit

---
+++ src/main/java/com/simple/simpleweb/SimplewebApplication.java
@@ -0,0 +1,12 @@
+package com.simple.simpleweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplewebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplewebApplication.class, args);
+	}
+}
